19a7c21be27e528fd67eea551b319cb587a4d480
RT <beat1422@icloud.com>
2019-11-01 22:45:43 -0700

    easiest question

--- 8kyu.js
+++ 8kyu.js
@@ -168,3 +168,23 @@ function makeUpperCase(str) {
   var res = str.toUpperCase();
   return res;
 }
+
+/*9. Grasshopper - Messi goals function----------------------------------------
+// Messi is a soccer player with goals in three leagues:
+
+LaLiga
+Copa del Rey
+Champions
+Complete the function to return his total number of goals in all three leagues.
+
+Note: the input will always be valid.
+
+For example:
+
+5, 10, 2  -->  17
+*/
+
+// my answer
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+}
