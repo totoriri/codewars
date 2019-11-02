5d8bc9556b5b696d1fddd22879a1f78997e3c266
RT <lemlem11217@gmail.com>
2019-11-01 21:57:16 -0700

    solved easiest 2 question

--- 8kyu.js
+++ 8kyu.js
@@ -101,3 +101,38 @@ function checkAlive(health) {
 
 //better answer(arrow function)
 const checkAlive = health => health > 0;
+
+
+/*5.Personalized greeting ----------------------------------------
+// Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+// Use conditionals to return the proper message.
+*/
+
+// my answer
+function greet (name, owner) {
+  if (name === owner) {
+    return 'Hello boss';
+  }
+  return 'Hello guest';
+}
+
+// better answer
+const greet = (name, owner) => `Hello ${name === owner ? 'boss' : 'guest'}`;
+
+
+/*6.Convert a string to an array ----------------------------------------
+// Write a function to split a string and convert it into an array of words.
+ For example:
+ "Robin Singh" ==> ["Robin", "Singh"]
+
+"I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+*/
+
+// my answer
+function stringToArray(string){
+ return string.split(' ');
+}
+stringToArray("Robin Singh")
+
+// other answer
+let stringToArray = string => string.split(' ');
